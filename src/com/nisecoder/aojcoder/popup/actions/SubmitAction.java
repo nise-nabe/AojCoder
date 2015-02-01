5884fa3c962de44fc7d5c0fa8c5c5b3dfb2c9050
nise_nabe <nise.nabe@gmail.com>
2015-02-01 16:39:41 +0900

    fix comment

--- src/com/nisecoder/aojcoder/popup/actions/SubmitAction.java
+++ src/com/nisecoder/aojcoder/popup/actions/SubmitAction.java
@@ -28,7 +28,7 @@ public class SubmitAction implements IObjectActionDelegate {
 	private Shell shell;
 
 	/**
-	 * Constructor for Action1.
+	 * Constructor for SubmitAction
 	 */
 	public SubmitAction() {
 		super();
